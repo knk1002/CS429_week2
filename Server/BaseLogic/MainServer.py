e9604cdb5599cdbd3585879bf310c33170b91d00
kjjo95 <kyungjejo@kaist.ac.kr>
2016-01-05 18:16:24 +0900

    Both Player Loaded

--- Server/BaseLogic/MainServer.py
+++ Server/BaseLogic/MainServer.py
@@ -7,8 +7,8 @@ import socket
 import time
 from time import ctime
 
-# Host = '143.248.36.223'
-Host = '127.0.0.1'
+Host = '143.248.225.132'
+#Host = '127.0.0.1'
 Port = 2345
 Bufsize = 1024
 ADDR = (Host,Port)
@@ -38,61 +38,61 @@ class client_sock:
 
 
 def recv_json(sock,room_num,net_order):
-    while True:
-        try:
-            data = clientsock.recv(Bufsize)
-        except socket.error, e:
-            pass
-        client_socklist = player_pair_list[room_num]
-        if checktype(data) == "Move":
-            if net_order == 1:
-                client_socklist.firstqueue.append([data,client_socklist.second])
-            else:
-                client_socklist.secondqueue.append([data,client_socklist.first])
-        elif checktype(data) == "Load":
-            if net_order == 1:
-                client_socklist.set_load_1P()
-            else:
-                client_socklist.set.load_2P()
-            if client_socklist.firstload == True and client_socklist.secondload == True:
-                client_socklist.firstqueue.append([_Serializer("Start",'',-1),client_socklist.first])
-                client_socklist.secondqueue.append([_Serializer("Start",'',-1),client_socklist.second])
-        print "Data send to opponent successfully"
-
-        time.sleep(0)
+    i =0
+    try:
+        while True:
+            data = sock.recv(Bufsize)
+            client_socklist = player_pair_list[room_num]
+            if checktype(data) == "Move":
+                if net_order == 1:
+                    client_socklist.firstqueue.append([data,client_socklist.second])
+                else:
+                    client_socklist.secondqueue.append([data,client_socklist.first])
+            elif checktype(data) == "Load":
+                if net_order == 1:
+                    client_socklist.set_load_1P()
+                    while client_socklist.secondload == False:
+                        time.sleep(0)
+                else:
+                    client_socklist.set_load_2P()
+                if client_socklist.firstload == True and client_socklist.secondload == True:
+                    client_socklist.first.send(_Serializer(3,'',-1))
+                    client_socklist.second.send(_Serializer(3,'',-1))
+            print "Data send to opponent successfully"
+            time.sleep(0)
+    except socket.error, e:
+        pass
 
 def send_json(sock,room_num,net_order):
     client_socklist = player_pair_list[room_num]
+    i = 0
     while True:
-        if(net_order == 1):
-            if(not client_socklist.firstqueue == []):
-                client_socklist.firstqueue[0][1].send(client_socklist.firstqueue[0][0])
-                del client_socklist.firstqueue[0]
-        else:
+        if(net_order == 2):
             if(not client_socklist.secondqueue == []):
                 client_socklist.secondqueue[0][1].send(client_socklist.secondqueue[0][0])
                 del client_socklist.secondqueue[0]
+
+        else:
+            if(not client_socklist.firstqueue == []):
+                client_socklist.firstqueue[0][1].send(client_socklist.firstqueue[0][0])
+                del client_socklist.firstqueue[0]
         time.sleep(0)
 
 
 
 def ClientThread(clientsock,addr,room_num,net_order):
     thread.start_new_thread(recv_json,(clientsock,room_num,net_order))
+    while player_pair_list[room_num].firstload == False or player_pair_list[room_num].secondload == False:
+        time.sleep(0)
     thread.start_new_thread(send_json,(clientsock,room_num,net_order))
 
-    client_socklist = player_pair_list[room_num]
-
-    if(not(player_pair_list[room_num].first == None) and not(player_pair_list[room_num].second == None)):
-        client_socklist.firstqueue.append([_Serializer("Connect",[],-1),client_socklist.first])
-        print "Yeah"
-        client_socklist.secondqueue.append([_Serializer("Connect",[],-1),client_socklist.second])
 
 def checktype(msg):
     js  = json.loads(msg)
     _Type = js['Type']
-    if (_Type == 'MoveStart') or (_Type == 'MoveEnd'):
+    if (_Type == 4) or (_Type == 5):
         return "Move"
-    elif (_Type == 'Load'):
+    elif (_Type == 2):
         return 'Load'
 
 
@@ -121,6 +121,10 @@ if __name__ == '__main__':
             player_pair_list[room_num].first = clientsock
         else:
             player_pair_list[room_num].second = clientsock
+            client_socklist = player_pair_list[room_num]
+            if(not(player_pair_list[room_num].first == None) and not(player_pair_list[room_num].second == None)):
+                client_socklist.first.send(_Serializer(1,[],-1))
+                client_socklist.second.send(_Serializer(1,[],-1))
 
         clientsock.send(_Serializer("PlayerOrder",[net_order],-1))
         thread.start_new_thread(ClientThread, (clientsock, addr,room_num,net_order))
